c3712af0824c33b38a8e74707713b3a1d09fd024
YHW <ymfsder@YHWdeMacBook-Pro.local>
2021-12-08 13:54:03 +0800

    project init

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+const Login = () => import(/* webpackChunkName: 'login' */'../views/Login')
+const Register = () => import(/* webpackChunkName: 'register' */'../views/Register')
+const Forget = () => import(/* webpackChunkName: 'forget' */'../views/Forget')
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    component: Login
+  }, {
+    path: '/register',
+    name: 'register',
+    component: Register
+  }, {
+    path: '/forget',
+    name: 'forget',
+    component: Forget
   }
+
 ]
 
 const router = new VueRouter({
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,121 @@
 <template>
-  <div>
-    login
+  <div class="layui-container fly-marginTop">
+    <div class="fly-panel fly-panel-user" pad20>
+      <div class="layui-tab layui-tab-brief" lay-filter="user">
+        <ul class="layui-tab-title">
+          <li class="layui-this">登入</li>
+          <li>
+            <router-link :to="{name:'register'}">注册</router-link>
+          </li>
+        </ul>
+        <div class="layui-form layui-tab-content" id="LAY_ucm" style="padding: 20px 0;">
+          <div class="layui-tab-item layui-show">
+            <div class="layui-form layui-form-pane">
+              <form method="post">
+                <validation-provider v-slot="{errors}" name="用户名" rules="required|email">
+                  <div class="layui-form-item">
+                    <label class="layui-form-label">用户名</label>
+                    <div class="layui-input-inline">
+                      <input type="text" name="username" v-model="username" placeholder="请输入用户名" autocomplete="off"
+                             class="layui-input">
+                    </div>
+                    <div class="layui-form-mid err">{{ errors[0] }}</div>
+                  </div>
+                </validation-provider>
+                <validation-provider v-slot="{errors}" name="密码" rules="required|min:6">
+                  <div class="layui-form-item">
+                    <label for="L_pass" class="layui-form-label">密码</label>
+                    <div class="layui-input-inline">
+                      <input type="password" v-model="password" id="L_pass" name="password" autocomplete="off"
+                             class="layui-input">
+                    </div>
+                    <div class="layui-form-mid err">{{ errors[0] }}</div>
+                  </div>
+                </validation-provider>
+                <validation-provider v-slot="{errors}" name="验证码" rules="required|length:4">
+                  <div class="layui-form-item">
+                    <label for="L_vercode" class="layui-form-label">验证码</label>
+                    <div class="layui-input-inline">
+                      <input type="text" id="L_vercode" name="code" v-model="code"
+                             placeholder="请输入验证码" autocomplete="off" class="layui-input">
+                    </div>
+                    <div class="layui-form-mid svg-code" @click="_getCode">
+                      <span v-html="svg"></span>
+                    </div>
+                    <div class="layui-form-mid err">{{ errors[0] }}</div>
+                  </div>
+                </validation-provider>
+                <div class="layui-form-item">
+                  <button class="layui-btn" lay-filter="*" lay-submit>立即登录</button>
+                  <span style="padding-left:20px;">
+                  <router-link :to="{name: 'forget'}">忘记密码？</router-link>
+                </span>
+                </div>
+                <div class="layui-form-item fly-form-app">
+                  <span>或者使用社交账号登入</span>
+                  <a href="" onclick="layer.msg('正在通过QQ登入', {icon:16, shade: 0.1, time:0})" class="iconfont icon-qq"
+                     title="QQ登入"></a>
+                  <a href="" onclick="layer.msg('正在通过微博登入', {icon:16, shade: 0.1, time:0})"
+                     class="iconfont icon-weibo"
+                     title="微博登入"></a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { ValidationProvider, extend } from 'vee-validate'
+import * as rules from 'vee-validate/dist/rules'
+import { messages } from 'vee-validate/dist/locale/zh_CN.json'
+import { getCode } from '../api/login'
+
+Object.keys(rules).forEach(rule => {
+  extend(rule, {
+    ...rules[rule],
+    message: messages[rule]
+  })
+})
+
 export default {
-  name: 'login'
+  name: 'login',
+  data: function () {
+    return {
+      username: '',
+      password: '',
+      code: '',
+      svg: ''
+    }
+  },
+  components: {
+    ValidationProvider
+  },
+  mounted () {
+    this._getCode()
+  },
+  methods: {
+    _getCode () {
+      getCode().then((res) => {
+        if (res.code === 200) {
+          this.svg = res.data
+        }
+      })
+    }
+  }
 }
 </script>
 
 <style scoped>
+.svg-code {
+  position: relative;
+  top: -20px;
+}
 
+.err {
+  color: red;
+}
 </style>
